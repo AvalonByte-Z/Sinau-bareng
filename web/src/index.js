43ab70c96cbcfe5fff813c32c04380b1b728253f
thoriqnf <thoriqnfaizal@gmail.com>
2017-11-12 23:14:01 +0700

    finishing index.js at web

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import react
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import for styling
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+// import for app
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
