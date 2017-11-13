0cc0903b89bb3c2a96bbdba428b25788b2e1679e
thoriqnf <thoriqnfaizal@gmail.com>
2017-11-13 08:24:27 +0700

    creating footer

---
+++ web/src/organisms/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const style = {
+  marginTop: "20px",
+  bprderTop: "1px solid black"
+};
+
+const Footer = () => (
+  <div style={style}>
+    <small>2017 &copy; Thq and Hafidz</small>
+  </div>
+);
