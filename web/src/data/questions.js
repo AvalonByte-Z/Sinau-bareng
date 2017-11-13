ee0dae2ee29d2b321254346847868e60088cd208
thoriqnf <thoriqnfaizal@gmail.com>
2017-11-13 08:21:25 +0700

    adding data folder including question and users

---
+++ web/src/data/questions.js
@@ -0,0 +1,28 @@
+module.exports = [
+  {
+    _id: 1,
+    title: "Berapa 1 + 1= ?",
+    createdBy: {
+      _id: 1,
+      name: "Thoriq1"
+    },
+    answers: [
+      {
+        _id: 0,
+        text: "Jawabannya 2 bro",
+        createdBy: {
+          _id: 0,
+          name: "Thoriq2"
+        }
+      },
+      {
+        _id: 1,
+        text: "bukan bro Jawabannya 1,1",
+        createdBy: {
+          _id: 1,
+          name: "Thoriq"
+        }
+      }
+    ]
+  }
+];
