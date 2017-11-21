37c79eb01c9778d6be7e7d899efb37072eb00ca6
thoriqnf <thoriqnfaizal@gmail.com>
2017-11-22 00:51:22 +0700

    moving to render column sign up and sign in

--- web/src/organisms/ColumnLogin.js
+++ web/src/organisms/ColumnLogin.js
@@ -8,7 +8,8 @@ import {
   Label,
   Input,
   CardBody
-} from "reactstrap";import axios from "axios"
+} from "reactstrap";
+import axios from "axios"
 
 import Link from "../atoms/Link"
 
@@ -25,45 +26,50 @@ const styleButton = {
   bottom: "10px"
 };
 
-const ColumnLogin = () => (
-  <Col xs={12}>
-    <CardBody>
-      <h3>Login</h3>
-      <Form style={styleForm}>
-        <FormGroup>
-          <Label for="loginEmail">Email</Label>
-          <Input
-            type="email"
-            name="loginEmail"
-            id="loginEmail"
-            placeholder="Enter your email here"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="loginPassword">Password</Label>
-          <Input
-            type="password"
-            name="loginPassword"
-            id="loginPassword"
-            placeholder="Enter your password here"
-          />
-        </FormGroup>
-        <br />
-        <FormGroup>
-          <Link to="/forgot">Forgot password?</Link>
-        </FormGroup>
-        <br />
-        <br />
-        <Row>
-          <Col xs={6}>
-            <Button style={styleButton} color="success">
-              Login
-            </Button>
-          </Col>
-        </Row>
-      </Form>
-    </CardBody>
-  </Col>
-);
+
+class ColumnLogin extends React.Component {
+  render() {
+    return (
+      <Col xs={12}>
+        <CardBody>
+          <h3>Login</h3>
+          <Form style={styleForm}>
+            <FormGroup>
+              <Label for="loginEmail">Email</Label>
+              <Input
+                type="email"
+                name="loginEmail"
+                id="loginEmail"
+                placeholder="Enter your email here"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="loginPassword">Password</Label>
+              <Input
+                type="password"
+                name="loginPassword"
+                id="loginPassword"
+                placeholder="Enter your password here"
+              />
+            </FormGroup>
+            <br />
+            <FormGroup>
+              <Link to="/forgot">Forgot password?</Link>
+            </FormGroup>
+            <br />
+            <br />
+            <Row>
+              <Col xs={6}>
+                <Button style={styleButton} color="success">
+                  Login
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </CardBody>
+      </Col>
+    )
+  }
+}
 
 export default ColumnLogin
--- web/src/organisms/ColumnSignup.js
+++ web/src/organisms/ColumnSignup.js
@@ -14,42 +14,45 @@ const styleForm = {
   height: "350px"
 };
 
-const ColumnSignup = () => (
-  <Col xs={12}>
-    <CardBody>
-      <h3>Sign Up</h3>
-      <Form style={styleForm}>
-        <FormGroup>
-          <Label for="signupFullName">Full Name</Label>
-          <Input
-            type="email"
-            name="signupFullName"
-            id="signupFullName"
-            placeholder="Enter your fullname here"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="signupEmail">Email</Label>
-          <Input
-            type="email"
-            name="signupEmail"
-            id="signupEmail"
-            placeholder="Enter your email heree"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="signupPassword">Password</Label>
-          <Input
-            type="password"
-            name="signupPassword"
-            id="signupPassword"
-            placeholder="Enter your password here"
-          />
-        </FormGroup>
-        <Button>Sign Up</Button>
-      </Form>
-    </CardBody>
-  </Col>
-);
+export default class ColumnSignup extends React.Component{
 
-export default ColumnSignup
+  render(){
+    return (
+      <Col xs={12}>
+        <CardBody>
+          <h3>Sign Up</h3>
+          <Form style={styleForm}>
+            <FormGroup>
+              <Label for="signupFullName">Full Name</Label>
+              <Input
+                type="email"
+                name="signupFullName"
+                id="signupFullName"
+                placeholder="Enter your fullname here"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="signupEmail">Email</Label>
+              <Input
+                type="email"
+                name="signupEmail"
+                id="signupEmail"
+                placeholder="Enter your email heree"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="signupPassword">Password</Label>
+              <Input
+                type="password"
+                name="signupPassword"
+                id="signupPassword"
+                placeholder="Enter your password here"
+              />
+            </FormGroup>
+            <Button>Sign Up</Button>
+          </Form>
+        </CardBody>
+      </Col>
+    )
+  }
+}
