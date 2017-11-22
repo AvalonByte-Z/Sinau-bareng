6faef53a307e7309316d4071d210aa94ff0e12f5
thoriqnf <thoriqnfaizal@gmail.com>
2017-11-22 18:56:24 +0700

    finishing list questions

--- web/src/pages/ListQuestion.js
+++ web/src/pages/ListQuestion.js
@@ -1,22 +1,11 @@
 import React from "react";
 
-import LinkToProfile from "../atoms/LinkToProfile";
+import ListOfQuestions from "../organisms/ListOfQuestions";
 import Layout from "../templates/Layout";
-
-import DATAPROFILES from "../data/users";
 
 const ListQuestion = () => (
   <Layout>
-    <ul>
-      {DATAPROFILES.map(user => {
-        console.log(user);
-        return (
-          <li>
-            <LinkToProfile user={user} />
-          </li>
-        );
-      })}
-    </ul>
+    <ListOfQuestions></ListOfQuestions>
   </Layout>
 );
 
